7ab0740d0f8b5bb686ac551c5b5bec7055f4f307
Caio Cesar <ccesar@lenovo.com>
2020-02-13 15:57:29 -0300

    feat: added generate media to makefile

--- Files/MyHelloWorld.c
+++ Files/MyHelloWorld.c
@@ -21,11 +21,11 @@
 **/
 EFI_STATUS
 EFIAPI
-UefiMain (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
-  )
+UefiMain(
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable)
 {
-  Print(L"Hello World \n"); 
+  Print(L"Hello World!");
+
   return EFI_SUCCESS;
 }
